3726309f3659dae234c0b3394341aec3605ea75c
Kulpot <kulpotwahu@gmail.com>
2023-07-14 02:29:19 +0800

    Add project files.

---
+++ Hello World Thread/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+//ref link:https://www.youtube.com/watch?v=-s8dOv5G8WA&list=PLRwVmtr-pp06KcX24ycbC-KkmAISAFKV5
+// Threading -- Basic
+
+class MainClass
+{
+    static void Main()
+    {
+        //Console.WriteLine("My Name");
+        var thread = new Thread(DifferentMethod);
+        thread.Start();
+    }
+    static void DifferentMethod()
+    {
+        Console.WriteLine("My Name");
+    }
+}
